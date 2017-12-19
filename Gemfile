7c72b4cacdf7ad687d7be061970ea6f369d4db28
Mark Struzinski <mark.struzinski@lowes.com>
2017-12-19 13:37:57 -0500

    Adding site association file

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails', '4.3.1'
 
+gem 'apple-app-site-association', '~> 0.1.2'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'capybara', '~> 2.13'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  mount Apple::App::Site::Association, at: '/'
 end
